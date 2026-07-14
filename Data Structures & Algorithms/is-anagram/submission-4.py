2cd97e75901c148648e57b663318fba0ca294cd4
Rookie <136296017+RookiexCookie@users.noreply.github.com>
2026-07-14 09:06:04 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        init_ch = ord('a')
+        helper = [0]*26
+        for ch1 in s:
+            helper[ord(ch1) - init_ch] += 1
+        for ch2 in t:
+            if helper[ord(ch2) - init_ch] == 0:
+                return False
+            helper[ord(ch2) - init_ch] -= 1
+        return sum(helper) == 0
